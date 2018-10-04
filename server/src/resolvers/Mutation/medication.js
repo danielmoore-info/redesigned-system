d0850542616e90ad71a36ee610c43297d9da1d22
Daniel Moore <danielmoore.info@gmail.com>
2018-10-04 12:23:44 +1000

    Implemented Update Medication

--- server/src/resolvers/Mutation/medication.js
+++ server/src/resolvers/Mutation/medication.js
@@ -16,6 +16,29 @@ const medication = {
       info
     )
   },
+
+  async updateMedication(parent, {id, name, count}, ctx, info) {
+    const userId = getUserId(ctx)
+    const medicationExists = await ctx.db.exists.Medication({
+      id,
+      patient: {
+        id: userId
+      },
+    })
+    if (!medicationExists) {
+      throw new Error(`Medication not found`)
+    }
+    return ctx.db.mutation.updateMedication(
+      {
+        where:{id},
+        data: {
+          name: name,
+          count: count,
+        }
+      },
+      info
+    )
+  },
 }   
 
 module.exports = {medication}
